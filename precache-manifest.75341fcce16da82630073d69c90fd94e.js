b2e25d6a01992fdb95953e007711b96865a8c4dd
Code Sandbox <hello@codesandbox.io>
2021-11-12 15:15:45 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.75341fcce16da82630073d69c90fd94e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41ccfddfa2cf95791cb9",
+    "url": "https://arielschwalb.github.io/csb-83be7/static/css/main.3e8580d3.chunk.css"
+  },
+  {
+    "revision": "41ccfddfa2cf95791cb9",
+    "url": "https://arielschwalb.github.io/csb-83be7/static/js/main.2c26b427.chunk.js"
+  },
+  {
+    "revision": "2dbc3a52ef6019e738b8",
+    "url": "https://arielschwalb.github.io/csb-83be7/static/js/runtime~main.7e000535.js"
+  },
+  {
+    "revision": "4f1e19e6e07ead45ea90",
+    "url": "https://arielschwalb.github.io/csb-83be7/static/js/2.f3d7e44e.chunk.js"
+  },
+  {
+    "revision": "780fd7a95f35545929460372ab23d67c",
+    "url": "https://arielschwalb.github.io/csb-83be7/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://arielschwalb.github.io/csb-83be7/precache-manifest.75341fcce16da82630073d69c90fd94e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://arielschwalb.github.io/csb-83be7/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
